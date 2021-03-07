95d4fc1dddd456ea989f904d5df14ac4ba43d3c0
student <ednnamukami@gmail.com>
2021-03-07 09:30:25 +0300

    master

---
+++ assignment.js
@@ -0,0 +1,6 @@
+function fruits(){
+    fruits_list=["grapes","pineaples","mango","orange","tomato"];
+    fruits_list.forEach(element => console.log(element))
+console.log(fruits_list[3])
+}
+fruits();
